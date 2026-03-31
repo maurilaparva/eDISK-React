71f44b9d14a0137fca8fe468560feedefe442b1c
Mauricio <villa669@umn.edu>
2026-03-30 19:53:20 -0500

    Vercel changes

--- eDISK_UI/edisk-react/vite.config.js
+++ eDISK_UI/edisk-react/vite.config.js
@@ -41,6 +41,6 @@ export default defineConfig(({ mode }) => {
         },
 
     // For GitHub Pages, assets must be loaded from /REPO_NAME/ subpath
-    base: isGHPages ? '/eDISK-React/' : '/',
+    base: isGHPages ? '/' : '/',
   };
 });
